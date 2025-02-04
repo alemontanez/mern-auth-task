3ea4d2cb6cb036c8afdf715996b69a0b2c518619
Ale <alemontanez99@gmail.com>
2025-02-03 21:26:33 -0300

    Se agrega "Authorization" a allowedHeaders en la config de cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors({
   origin: FRONTEND_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }))
 
